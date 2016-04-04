aeb4db4e1a20e04efc6f85fd797da80bed748784
como89 <como89.dev@gmail.com>
2016-04-04 19:20:06 -0400

    Update pom.xml
    Update pom.xml to 2.0 version of MyApi and
    finish correctly the javadoc of the api with the class Operator.

--- src/ca/como89/myapi/api/queries/Operator.java
+++ src/ca/como89/myapi/api/queries/Operator.java
@@ -1,8 +1,18 @@
 package ca.como89.myapi.api.queries;
 
+/**
+ * This enum class is when you want to add between two conditions an operator.
+ * This enum is needed for multi conditions.
+ */
 public enum Operator {
 	
+	/**
+	 * The operator AND.
+	 */
 	AND("AND"),
+	/**
+	 * The operator OR.
+	 */
 	OR("OR");
 	
 	private String operator;
